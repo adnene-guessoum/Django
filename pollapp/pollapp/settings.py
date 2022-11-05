e826fe4d0f977372c5382905dab33cbf190c6945
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-05 13:11:03 +0100

    Change secret key

--- pollapp/pollapp/settings.py
+++ pollapp/pollapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-97gvb+w*p#_!^fg*e(+3cs^_(k=+tcy*7%-m80lmxf0c*z-2ay'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+01:00'
 
 USE_I18N = True
 
