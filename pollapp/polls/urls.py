25816a0f17b09411fc779dd922703538788832c1
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-10 15:08:03 +0100

    Use generic views for django poll app

--- pollapp/polls/urls.py
+++ pollapp/polls/urls.py
@@ -5,12 +5,8 @@ from . import views
 # named templates/views in different apps inside the same django project
 app_name = 'polls'
 urlpatterns = [
-			# index /polls/
-			path('', views.index, name='index'),
-			# index /polls/5/ (for question_id 5)
-			path('<int:question_id>/', views.detail, name='detail'),
-			# index /polls/5/results/
-			path('<int:question_id>/results/', views.results, name='results'),
-			# index /polls/5/vote/
-			path('<int:question_id>/vote/', views.vote, name='vote'),
+		path('', views.IndexView.as_view(), name='index'),
+		path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+		path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+		path('<int:question_id>/vote/', views.vote, name='vote'),
 		]
