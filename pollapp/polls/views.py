283e3b7da21f658056a5c3dde1a69f6b618b6657
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-10 13:18:08 +0100

    Add views for votes and results based on Form

--- pollapp/polls/views.py
+++ pollapp/polls/views.py
@@ -1,20 +1,12 @@
-from django.http import HttpResponse, Http404
-
-#import our model classes
-from .models import Question
-
-#import our templates to display views
-#from django.template import loader
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 # Create the views
 def index(request):
 
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
-	#enough if directory properly namespaced
-	#template = loader.get_template('polls/index.html')
-
 	#passing the required context to be displayed by our template in the view
 	context = {
 			'latest_question_list': latest_question_list,
@@ -22,7 +14,34 @@ def index(request):
 
 	return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):  
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# display the form again
+		return render(request, 'polls/detail.html', {'question':question,
+			'error_message': "You didn't select a choice.",})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		#Best practice: always return an HttpResponseRedirect after successfull
+		#post data to prevent posting twice on back button 
+	return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
+
+
 '''
+#other 404 option to reduce code
 def detail(request, question_id):
 	try:
 		question = Question.objects.get(pk=question_id)
@@ -30,15 +49,4 @@ def detail(request, question_id):
 		raise Http404("Question does not exist")
 	return render(request, 'polls/detail.html', {'question':question})
 '''
-#other 404 option to reduce code
-from django.shortcuts import get_object_or_404
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):  
-	response = "You're looking at the results of question %s" 
-	return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
